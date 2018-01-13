12204767647a2070228a603d3ea8cef80446a9ee
Estelle Yang <estelleyyy@Estelles-MacBook-Air.local>
2018-01-13 15:15:47 -0500

    Project Created

---
+++ app/src/main/java/com/example/estelleyyy/clinic_in_a_box/Finish.java
@@ -0,0 +1,13 @@
+package com.example.estelleyyy.clinic_in_a_box;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Finish extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish);
+    }
+}
