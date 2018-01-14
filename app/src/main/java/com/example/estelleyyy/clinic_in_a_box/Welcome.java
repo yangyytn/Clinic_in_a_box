7046535a0891f2de6b342a9415e3c873f0c7d518
maowenyu <wenyu.mao@mail.utoronto.ca>
2018-01-13 19:47:50 -0500

    Connect welcome to login, login to questionnaire

--- app/src/main/java/com/example/estelleyyy/clinic_in_a_box/Welcome.java
+++ app/src/main/java/com/example/estelleyyy/clinic_in_a_box/Welcome.java
@@ -1,7 +1,9 @@
 package com.example.estelleyyy.clinic_in_a_box;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Welcome extends AppCompatActivity {
 
@@ -10,4 +12,9 @@ public class Welcome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
     }
+    public void goToNext(View v){
+        Intent startNewActivity = new Intent(this, Login.class);
+        startActivity(startNewActivity);
+    }
+
 }
