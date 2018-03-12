de4292a97485df330bac5e371d10985affa28013
wendy <wendyqiu1995@yahoo.ca>
2018-03-11 22:46:36 -0400

    added radio groups (single selection, mutual exclusive) # questionnaire.xml

---
+++ app/src/main/java/com/example/estelleyyy/clinic_in_a_box/DiagnosisAlgorithm.java
@@ -0,0 +1,15 @@
+package com.example.estelleyyy.clinic_in_a_box;
+
+/**
+ * Created by wendy on 2018-03-11.
+ */
+
+public class DiagnosisAlgorithm {
+
+    public void RiskPercentAlg() {
+
+        // based on questionnaire answers:
+
+        //based on tests
+    }
+}
