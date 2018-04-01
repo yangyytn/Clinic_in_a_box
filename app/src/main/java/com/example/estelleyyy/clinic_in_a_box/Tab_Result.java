90258b176a72800101c65967b62e01b50f6bcf2c
wendy <wendyqiu1995@yahoo.ca>
2018-04-01 01:56:39 -0400

    display results;

--- app/src/main/java/com/example/estelleyyy/clinic_in_a_box/Tab_Result.java
+++ app/src/main/java/com/example/estelleyyy/clinic_in_a_box/Tab_Result.java
@@ -22,16 +22,7 @@ public class Tab_Result extends Fragment {
 
         // connect this to the XML!!!
         // use the below 2 lines for results display!!!!!!
-        /*
 
-
-        // globally
-        String output = "hehe";
-        TextView textView1;
-        //in your OnCreate() method
-        textView1 = getView().findViewById(R.id.textView10);
-        textView1.setText(output);
-*/
         int[] Qresults = new int[5];
         ((GlobalVariables) this.getActivity().getApplication()).getQresult(Qresults);
 
@@ -40,10 +31,48 @@ public class Tab_Result extends Fragment {
             System.out.println("Qresult is: " + Qresults[i]);
         }
 
-        String output = CurrentTestResult(Qresults);    //have problems
+        double Result = CurrentTestResult(Qresults);
 
-        TextView textView1 = rootView.findViewById(R.id.textView10);
-        textView1.setText(output);
+        String output1 =Result + "%";
+        String output2="";
+
+        if (Result<25) {
+            output2+="Low Risk Percentage";
+        }
+        else if (Result>70) {
+            output2+="High Risk Percentage";
+        }
+        else {
+            output2+="Moderate Risk Percentage";
+        }
+
+        //display the physical test measurements:
+        String test1="";
+        String test2="";
+        String test3="";
+        double[] BloodPressure = ((GlobalVariables) this.getActivity().getApplication()).getBloodPressure();
+        double BloodOxy = ((GlobalVariables) this.getActivity().getApplication()).getOxygen();
+        double temp = ((GlobalVariables) this.getActivity().getApplication()).getTemp();
+        //output+="Below shows the physical test results for the current patient: .\n";
+        test1+="Systolic Pressure: " + BloodPressure[0];
+        test1+="\nDiastolic Pressure: " + BloodPressure[1];
+        test2+=BloodOxy + "%";
+        test3+=temp + "°C";
+
+        TextView textView1 = rootView.findViewById(R.id.textView11);
+        textView1.setText(output1);
+
+        TextView textView2 = rootView.findViewById(R.id.textView12);
+        textView2.setText(output2);
+
+        TextView textView3 = rootView.findViewById(R.id.textView13);
+        textView3.setText(test1);
+
+        TextView textView4 = rootView.findViewById(R.id.textView14);
+        textView4.setText(test2);
+
+        TextView textView5 = rootView.findViewById(R.id.textView15);
+        textView5.setText(test3);
 
         return rootView;
     }
@@ -51,7 +80,8 @@ public class Tab_Result extends Fragment {
 
 
     // function to display (current) test result
-    String CurrentTestResult(int[] Qresults) {
+    //分开variables来return,,,还没做完
+    double CurrentTestResult(int[] Qresults) {
 
         double result;
         int age = ((GlobalVariables) this.getActivity().getApplication()).getAge();
@@ -65,30 +95,10 @@ public class Tab_Result extends Fragment {
         //set risk percentage to global variable
         ((GlobalVariables) this.getActivity().getApplication()).setRisk(result);
 
-        String output="The risk percentage is: " + result + "%. \n";
-        if (result<25) {
-            output+="The patient has a relatively low chance of getting Sepsis.\n";
-        }
-        else if (result>70) {
-            output+="The patient has a relatively high chance of getting Sepsis.\n";
-            output+="This is urgent. Please go to the local hospital as soon as possible.\n";
-        }
-        else {
-            output+="Moderate risk of Sepsis.\n";
-            output+="The patient is strongly recommended to take a thorough examination.\n";
-        }
+        return result;
 
-        //display the physical test measurements:
-        double[] BloodPressure = ((GlobalVariables) this.getActivity().getApplication()).getBloodPressure();
-        double BloodOxy = ((GlobalVariables) this.getActivity().getApplication()).getOxygen();
-        double temp = ((GlobalVariables) this.getActivity().getApplication()).getTemp();
-        output+="Below shows the physical test results for the current patient: .\n";
-        output+="Systolic Pressure: " + BloodPressure[0];
-        output+="\nDiastolic Pressure: " + BloodPressure[1];
-        output+="\nBlood Oxygen: " + BloodOxy;
-        output+="\nBody Temperature: " + temp;
 
-        return output;
+        //return output; //todo: output1, output2, test1, test2, test3;  //need to change
 
     }
 
